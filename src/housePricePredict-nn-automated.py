b60f2365fd3532e7fbe5ac8a792250a3dd7cc31a
jparep <parepson@gmail.com>
2024-03-30 14:10:47 -0600

    Model evaluation

--- src/housePricePredict-nn-automated.py
+++ src/housePricePredict-nn-automated.py
@@ -128,3 +128,8 @@ class HousePricePredictorClass:
         self.model_after_hyperTune = grid.best_estimator_
         self.best_params = grid.best_params_
         
+        def evaluate_model(self, model, X_test, y_test) -> None:
+            y_pred = model.predict(X_test)
+            mse = mean_square_error(y_test, y_pred)
+            r2 = r2_score(y_test, y_pred)
+            return mse, r2
