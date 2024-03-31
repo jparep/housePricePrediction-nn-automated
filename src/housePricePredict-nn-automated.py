33907cece42802114c218ffc17bd9a26e4005264
jparep <parepson@gmail.com>
2024-03-31 02:01:06 -0600

    Outlier class

--- src/housePricePredict-nn-automated.py
+++ src/housePricePredict-nn-automated.py
@@ -13,3 +13,28 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LeakyReLU
 from scikeras.wrappers import KerasRegressor
+
+
+from configuration import config
+
+logging.baseConfig(level=logging.INFO)
+
+# Handle Outliers class
+class OutlierHander(BaseEstimator, TransformerMixin):
+    def __init__(self, factor =1.5) -> None:
+        self.factor = factor
+    
+    def fir(self, X, y=None):
+        Q1 = X.quantile(0.25)
+        Q3 = X.quantile(0.75)
+        IQR = Q3 - Q1
+        self.lower_bound = Q1 - self.factor * IQR
+        self.upper_bound = Q3  + self.factor * IQR
+        return self
+    
+    def transform(self, X, y=None):
+        X = X.copy()
+        for col in X.columns:
+            X[col] = X[col].clip(lower=self.lower_bound[col], upper=self.upper_bound[col])
+        
+        return X
